d97a6346ceb29a5f8ef47a1d250d985f13f7e8cf
Dionne Memie <d.memie@gmail.com>
2024-08-06 12:31:51 +0100

    started ticket 2

--- scrabble-score.js
+++ scrabble-score.js
@@ -1,1 +1,12 @@
 export function calculateScrabbleScore(word) {}
+
+// 2. Now add the simplest code you can to the `calculateScrabbleScore` function. Make it return the right score for the word "A" to pass the failing test.
+//    - Once you've updated the implementation, run the tests again.
+//    - This time the test should pass (if your implementation is correct).
+//    - This is the "green" stage in the TDD cycle (red -> green -> refactor)
+// 3. Look over the code. Make changes to simplify or improve it if you can. When you're done with changes, run the tests again. This checks that your changes didn't break anything.
+
+function calculateScrabbleScore() {
+  let A = 1;
+  return A;
+}
--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -1,1 +1,12 @@
 import { calculateScrabbleScore } from "./scrabble-score";
+import { test } from "vitest";
+import { expect } from "vitest";
+
+// 1. Write a new test that checks whether `calculateScrabbleScore` correctly calculates the score for the word `A`.
+//    - Once you've written the test, run the tests with Vitest.
+//    - The test you wrote should fail at first. This is because you wrote the test before writing the code to make it pass.
+//    - This is the "red" stage in the TDD cycle (red -> green -> refactor)
+
+test("Score for A = 1", () => {
+  expect(calculateScrabbleScore(A).toEqual(1));
+});
